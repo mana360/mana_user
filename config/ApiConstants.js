7388b20b1a602d865aad12fac773fb33a26b9a3b
udayraj_etpl <udayraj.n@exceptionaire.co>
2020-03-20 11:26:42 +0530

    API constant added

---
+++ config/ApiConstants.js
@@ -0,0 +1,20 @@
+/**
+ * @author
+ * Ritesh Niwane
+ * 
+ */
+export default ApiConstants = {
+    example: 'example',
+    /* ----------Ritesh------------- */
+    //GET EXAMPLE
+    users: 'users?',
+    //POST EXAMPLE
+    login: 'login',
+
+
+
+    /* ----------DEV 2------------- */
+
+
+    /* ----------DEV 3------------- */
+}
