fd207d3ba4baf4d8a6da7f2d00d16d29fdddeb83
Ritesh Niwane <riteshniwane007@gmail.com>
2020-03-24 18:47:14 +0530

    dashboard api call added

--- views/WarehouseServices.js
+++ views/WarehouseServices.js
@@ -2,68 +2,98 @@
     design by -mayur
  */
 import React, { Component } from 'react';
-import { View, Text, Image, FlatList,TouchableOpacity } from 'react-native';
-import {  Card,CardItem} from "native-base";
+import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
+import { Card, CardItem } from "native-base";
 import { StyleTruckBooking } from '../config/CommonStyles';
 import Constants from '../config/Constants';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import { MainPresenter } from '../config/MainPresenter'
+import ApiConstants from '../config/ApiConstants';
 
 export default class WarehouseServices extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            warehouseServicesData:[
-                {title:"Current Trips",  type:"current",  desc:"fnsldfn fnsldfn fnsldfn fnsldfn fnsldfn lorempipsom", percent:75},
-                {title:"Upcoming Trips", type:"upcoming",  desc:"fnsldfn fnsldfn fnsldfn fnsldfn fnsldfn lorempipsom", percent:40},
+            warehouseServicesData: [
+                // {title:"Current Trips",  type:"current",  desc:"fnsldfn fnsldfn fnsldfn fnsldfn fnsldfn lorempipsom", percent:75},
+                // {title:"Upcoming Trips", type:"upcoming",  desc:"fnsldfn fnsldfn fnsldfn fnsldfn fnsldfn lorempipsom", percent:40},
             ],
+        }
+    }
+    componentDidMount() {
+        this.presenter.callGetApi(ApiConstants.getDashboardData, "", true)
+    }
+    onResponse(apiConstant, data) {
+        switch (apiConstant) {
+            case ApiConstants.getDashboardData:
+                if (data.status) {
+                    let current_trips = data.dashboard_data.warehouse_booking.current_trips
+                    let upcoming_trips = data.dashboard_data.warehouse_booking.upcoming_trips
+                    if (data && data.dashboard_data && data.dashboard_data.warehouse_booking) {
+                        let localArray = [
+                            { title: "Current Trips", type: "current", desc: data.dashboard_data.referral_content, percent:current_trips?current_trips :0  },
+                            { title: "Upcoming Trips", type: "upcoming", desc: data.dashboard_data.referral_content, percent:upcoming_trips?upcoming_trips :0  },
+                        ]
+                        this.setState({
+                            warehouseServicesData:localArray
+                        })
+                    }
+                } else {
+                    alert(data.message)
+                }
+                break;
+
+            default:
+                break;
         }
     }
     render() {
         let { navigation } = this.props
         return (
-            <View style={{ flex: 1,backgroundColor:Constants.COLOR_GREY }}>
+            <View style={{ flex: 1, backgroundColor: Constants.COLOR_GREY }}>
+                <MainPresenter ref={(ref) => { this.presenter = ref }} onResponse={this.onResponse.bind(this)} navigation={this.props.navigation} />
                 <FlatList
                     data={this.state.warehouseServicesData}
                     extraData={this.state}
-                    keyExtractor={(index)=>{index.toString()}}
+                    keyExtractor={(index) => { index.toString() }}
                     numColumns={1}
                     renderItem={
-                        ({item, index})=>
+                        ({ item, index }) =>
                             <View style={StyleTruckBooking.row}>
-                                 <View style={StyleTruckBooking.col1}>
-                                     <AnimatedCircularProgress
+                                <View style={StyleTruckBooking.col1}>
+                                    <AnimatedCircularProgress
                                         size={90}
                                         width={10}
                                         fill={item.percent}
-                                        rotation="90"
+                                        rotation="0"
                                         lineCap="round"
                                         duration={1200}
                                         tintColor="#9ABD08"
                                         backgroundColor="#E8E8E8">
-                                        { (fill) => ( <Text style={{color:"#9ABD08"}}> { item.percent } </Text>) }
+                                        {(fill) => (<Text style={{ color: "#9ABD08" }}> {item.percent} </Text>)}
                                     </AnimatedCircularProgress>
 
                                 </View>
                                 <View style={StyleTruckBooking.col2}>
-                                     <Text style={[StyleTruckBooking.labelText2]}>{item.title}</Text>
-                                     <Text style={StyleTruckBooking.descText}>{item.desc}</Text>
-                                     <TouchableOpacity style={StyleTruckBooking.button}
-                                              onPress={()=>{
-                                                  item.type=="current"
-                                                  ?
-                                                  this.props.navigation.navigate('WarehouseServicesCurrentTrip')
-                                                  :
-                                                  this.props.navigation.navigate('WarehouseServiceUpcomingTrip')
-                                              }} 
+                                    <Text style={[StyleTruckBooking.labelText2]}>{item.title}</Text>
+                                    <Text style={StyleTruckBooking.descText}>{item.desc}</Text>
+                                    <TouchableOpacity style={StyleTruckBooking.button}
+                                        onPress={() => {
+                                            item.type == "current"
+                                                ?
+                                                this.props.navigation.navigate('WarehouseServicesCurrentTrip')
+                                                :
+                                                this.props.navigation.navigate('WarehouseServiceUpcomingTrip')
+                                        }}
                                     >
-                                           <Text style={StyleTruckBooking.buttonLabel}>{Constants.ViewAll}</Text>
-                                     </TouchableOpacity>
+                                        <Text style={StyleTruckBooking.buttonLabel}>{Constants.ViewAll}</Text>
+                                    </TouchableOpacity>
                                 </View>
                             </View>
                     }
                 />
 
-           </View>
+            </View>
         )
     }
 }
