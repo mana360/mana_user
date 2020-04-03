a2259794f2c5128273ea0e052a067624221c782e
mayur satpaise <mayur.satpaise1@gmail.com>
2020-04-03 10:34:46 +0530

    change

--- views/SignUp.js
+++ views/SignUp.js
@@ -64,7 +64,7 @@ export default class SignUp extends Component {
         if (data.status) {  
       console.log(data);
           this.setState({otp_modal_visible:true, resp_otp_code:data.email_otp, resp_user_id:data.user_id});
-          await setUserData(data)
+          await setUserData(data.userData)
           this.timer = setInterval(()=>{
             this.setState({modalVisible_welcome:false})
             clearInterval(this.timer)
